8af14e9b6b06230ef494f92c6806d9ad43f39148
kracekumar <kracethekingmaker@gmail.com>
2011-10-24 02:35:49 +0530

    DOne with login

--- gummi/views.py
+++ gummi/views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for, session
 from flaskext.oauth import OAuth
 
 FACEBOOK_APP_ID = "111665765610828"
-FACEBOOK_APP_SECRET = " bb3f445f1a832bd24ba0a9bf2a0f5d63"
+FACEBOOK_APP_SECRET = "bb3f445f1a832bd24ba0a9bf2a0f5d63"
 
 oauth = OAuth()
 
@@ -18,18 +18,22 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
     )
 
+def get_user_name():
+    return session['user_name'] if session.has_key('user_name') else None
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user_name = get_user_name())
 
-@app.route('/chat', methods = ["POST", "GET"])
+@app.route('/chat/', methods = ["POST", "GET"])
 def chat():
     return render_template('chat.html')
 
-@app.route('/chatroom', methods = ['POST', 'GET'])
-def chatroom():
+@app.route('/chatroom/<name>/', methods = ['POST', 'GET'])
+def chatroom(name):
     return render_template('chatroom.html')
-@app.route('/login/', methods = ['post', 'get'])
+
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
     return facebook.authorize(callback=url_for('facebook_authorized',
            next=request.args.get('next') or request.referrer or None,
@@ -44,10 +48,15 @@ def facebook_authorized(resp):
             request.args['error_description'])
     session['oauth_token'] = (resp['access_token'], '')
     me = facebook.get('/me')
+    session['user_name'] = me.data['name']
+    return redirect(url_for('welcome'))
     return 'Logged in as id=%s name=%s redirect=%s' %\
-            (me.data['id'], me.data['name'], request.args.get('next'))
+           (me.data['id'], me.data['name'], request.args.get('next'))
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return session.get('oauth_token')
 
+@app.route('/login/welcome/')
+def welcome():
+    return session['user_name'] 
